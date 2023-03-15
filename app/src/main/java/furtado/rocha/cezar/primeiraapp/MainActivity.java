3bc5299a945f4966674ea9cd14a987ecb992a764
20211TIIMI0249 <paulocezarrf@hotmail.com>
2023-03-15 12:51:10 -0300

    Comentários das telas adicionados

--- app/src/main/java/furtado/rocha/cezar/primeiraapp/MainActivity.java
+++ app/src/main/java/furtado/rocha/cezar/primeiraapp/MainActivity.java
@@ -8,21 +8,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity { //A Classe MainActivity herda os elementos da Classe AddCompatActivy
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //O Metodo onCreate inicia assim que a tela e criada
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btnEnviar = findViewById(R.id.btnEnviar);
-        btnEnviar.setOnClickListener(new View.OnClickListener() {
+        Button btnEnviar = findViewById(R.id.btnEnviar); //Declara a variavel do tipo Button (btnEnviar) e busca o valor na interface referente ao id dessa variavel
+        btnEnviar.setOnClickListener(new View.OnClickListener() { //Metodo no qual atribui um comando ao botao que percebe clicks
             @Override
-            public void onClick(View view) {
-                EditText etDigiteAqui = findViewById(R.id.etDigiteAqui);
-                String texto = etDigiteAqui.getText().toString();
-                Intent intent = new Intent(MainActivity.this,NextActivity.class);
-                intent.putExtra("texto", texto);
-                startActivity(intent);
+            public void onClick(View view) { //Metodo que aciona assim que o Metodo setOnClickListener "ouve" um click
+                EditText etDigiteAqui = findViewById(R.id.etDigiteAqui); //Declara a variavel do tipo EditText (etDigiteAqui) e busca o valor na interface referente ao id dessa variavel
+                String texto = etDigiteAqui.getText().toString(); //Declara a variavel do tipo String (texto) e pega o texto escrito na variavel etDigiteAqui, convertendo para String
+                Intent intent = new Intent(MainActivity.this,NextActivity.class); // Declara uma Intecao (intent) que tem o objetivo de ir da MainActivity para a NextActivity
+                intent.putExtra("texto", texto); // Um metodo que além de ir para uma outra activity leva consigo os elementos citados no dicionario
+                startActivity(intent); //Inicia a intencao intent
             }
         });
     }
--- app/src/main/java/furtado/rocha/cezar/primeiraapp/NextActivity.java
+++ app/src/main/java/furtado/rocha/cezar/primeiraapp/NextActivity.java
@@ -6,15 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class NextActivity extends AppCompatActivity {
+public class NextActivity extends AppCompatActivity { /*A Classe NextActivity herda os elementos da Classe AddCompatActivy*/
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //O Metodo onCreate inicia assim que a tela e criada
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
-        Intent i = getIntent();
-        String text = i.getStringExtra("texto");
-        TextView tv = findViewById(R.id.tvExibir);
-        tv.setText(text);
+        Intent i = getIntent(); // Pega uma intencao e coloca na variavel i
+        String text = i.getStringExtra("texto"); // Pega o valor referente a chave "texto"
+        TextView tv = findViewById(R.id.tvExibir); //Declara a variavel do tipo TextView (tv) e busca o valor na interface referente ao id dessa variavel
+        tv.setText(text); // Coloca o valor buscado da intencao no TextView
     }
 }
